f67446627fb3dab40eb5b0bceb3b3bd173875ff5
marioloncarek <mario.loncarek@gmail.com>
2023-03-01 18:06:46 +0100

    typo fix

--- scripts/getPackageJson.js
+++ scripts/getPackageJson.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const path = require("path");
 
 /**
- * A module to get package informations from package.json
+ * A module to get package information's from package.json
  * @module getPackageJson
  * @param {...string} keys from package.json if no arguments passed it returns package.json content as object
  * @returns {object} with given keys or content of package.json as object
